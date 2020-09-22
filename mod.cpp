5688f57cd5acdd02c55aee2a435d9179c64e9340
Saborknight <support@saborknight.com>
2020-09-22 15:43:59 +1000

    Moved - All Songs over from 2BNB Extras
    Issue ID: N/A

---
+++ mod.cpp
@@ -0,0 +1,17 @@
+name = "2BNB Music";
+actionName = "Discord";
+action = "https://discord.gg/DRaWNyf";
+description = "2nd Battalion, Nord Brigade - Music";
+
+picture = "2bnb-logo.paa";
+contentBrowserPicture="2bnb-logo.paa";
+logo = "2bnb-logo.paa";
+logoOver = "2bnb-logo.paa";
+
+tooltip = "2nd Battalion, Nord Brigade - Music";
+tooltipOwned = "2nd Battalion, Nord Brigade - Music";
+overview = "2BNB extra customisations and enhancements.";
+author = "2nd Battalion, Nord Brigade Technical Team";
+overviewPicture = "2bnb-logo.paa";
+overviewText = "2nd Battalion, Nord Brigade - Music";
+overviewFootnote = "<br /><br /><t color='#999999'>This content is under APL-SA License.<t />";
