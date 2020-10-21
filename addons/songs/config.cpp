0a8dffcfba85508b994358dc9e8df239c6e1fd20
Saborknight <support@saborknight.com>
2020-10-21 14:59:27 +1100

    Clean up - Standardised CfgPatches & informational details
    Issue ID: N/A

--- addons/songs/config.cpp
+++ addons/songs/config.cpp
@@ -1,16 +1,28 @@
 class CfgPatches {
 	class bnb_m_songs {
+		// Addon identity
 		name = "2BNB Music - Songs";
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 1;
-		requiredAddons[] = {};
 		author = "2nd Battalion, Nord Brigade";
 		authors[] = {
-			"D. Ford",
+			"Ford",
 			"Arend",
 			"Wolfpack"
 		};
+		url = "https://discord.gg/DRaWNyf";
+		version = 1.0.0;
+		versionAr[] = {1,0,0,0};
+		versionStr = "v1.0.0";
+
+		// Addon requirements
+		requiredVersion = 2.00;
+		requiredAddons[] = {};
+
+		// Addon items
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		worlds[] = {};
 	};
 };
 
--- mod.cpp
+++ mod.cpp
@@ -4,13 +4,13 @@ action = "https://discord.gg/DRaWNyf";
 description = "2nd Battalion, Nord Brigade - Music";
 
 picture = "2bnb-logo.paa";
-contentBrowserPicture="2bnb-logo.paa";
+contentBrowserPicture = "2bnb-logo.paa";
 logo = "2bnb-logo.paa";
 logoOver = "2bnb-logo.paa";
 
 tooltip = "2nd Battalion, Nord Brigade - Music";
 tooltipOwned = "2nd Battalion, Nord Brigade - Music";
-overview = "2BNB extra customisations and enhancements.";
+overview = "2BNB music for the KLPQ Music Player.";
 author = "2nd Battalion, Nord Brigade Technical Team";
 overviewPicture = "2bnb-logo.paa";
 overviewText = "2nd Battalion, Nord Brigade - Music";
